54b283bac035a86d4465b326b3c6b21e740815f2
dirkbr <guppy.dirk@web.de>
2021-04-16 11:32:21 +0000

    Deploying to gh-pages from master @ 2371ca545426922e743804a697f8e39697cff717 🚀

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-(()=>{"use strict";console.log("Script started successfully"),WA.openCoWebSite("https://workadventu.re")})();
+(()=>{"use strict";console.log("Script started successfully"),WA.openCoWebSite("https://psyfako.org")})();
 //# sourceMappingURL=script.js.map
