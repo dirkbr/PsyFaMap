74e62a6d3c2133a4debd96ba99950186006835c2
Dirk Joachim Braun <dirk.joachim.braun@rwth-aachen.de>
2021-04-16 11:33:52 +0200

    *.tsx gelöscht

--- Strand.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="Strand" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../../Downloads/lpc-beach-desert 2/beach-desert.png" width="512" height="512"/>
-</tileset>
--- Strand2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="Strand2" tilewidth="32" tileheight="32" tilecount="204" columns="12">
- <image source="../../Downloads/Old.png" width="384" height="544"/>
-</tileset>
